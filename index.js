dcb1257bcde9155e2c26a61b31982cf8145166af
Jacob McFaul <mcjakedude@gmail.com>
2019-03-27 13:55:56 -0700

    CRUD finished, one last get command for both student and cohort

--- index.js
+++ index.js
@@ -25,6 +25,72 @@ server.get('/api/cohort', async (req, res) => {
   }
 });
 
+server.get('/api/cohort/:id', async (req, res) => {
+    try {
+      const cohort = await db('cohort')
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(cohort);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+  server.get('/api/cohort/:id/student', async (req, res) => {
+    try {
+      const cohort = await db('cohort')
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(cohort);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+  server.post('/api/cohort', async (req, res) => {
+    try {
+      const [id] = await db('cohort').insert(req.body);
+      const cohort = await db('cohort')
+        .where({ id })
+        .first();
+      res.status(201).json(cohort);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+  server.put('/api/cohort/:id', async (req, res) => {
+    try {
+      const count = await db('cohort')
+        .where({ id: req.params.id })
+        .update(req.body);
+  
+      if (count > 0) {
+        const cohort = await db('cohort')
+          .where({ id: req.params.id })
+          .first();
+  
+        res.status(200).json(cohort);
+      } else {
+        res.status(404).json(error);
+      }
+    } catch (error){}
+  });
+  
+  server.delete('/api/cohort/:id', async (req, res) => {
+    try {
+      const count = await db('cohort')
+        .where({ id: req.params.id })
+        .del();
+  
+      if (count > 0) {
+        res.status(204).end();
+      } else {
+        res.status(404).json(error);
+      }
+    } catch (error) {}
+  });
+  
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
   console.log(`API running on http://localhost:${port}`)
--- seeds/002-student.js
+++ seeds/002-student.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise) {
   return knex('student').truncate()
     .then(function () {
       return knex('student').insert([
-        {name: 'Zech'},
-        {name: 'Jake'},
-        {name: 'Manny'}
+        {name: 'Zech', cohortId: 3},
+        {name: 'Jake', cohortId: 3},
+        {name: 'Manny', cohortId: 2}
       ]);
     });
 };
